9f77368494120ee46b3a5f3f0119fdb074acf795
ebrardev <16155052@mersin.edu.tr>
2024-07-27 19:15:56 +0300

    add nativewind for style

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,13 @@
-import { Image, StyleSheet, Platform, View,Text } from 'react-native';
+import {  View,Text } from 'react-native';
 
 
 
 export default function HomeScreen() {
   return (
    
-    <View style={styles.container}>
-      <Text>Hello</Text>
+    <View className="flex-1 justify-center items-center">
+      <Text className='text-blue-600'>Hello</Text>
     </View>
   );
 }
 
- 
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:'center',
-    alignItems:'center'
-
-  }
-});
