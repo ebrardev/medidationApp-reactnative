c9fa214737804a322950f19ca2f4d8708e70c87f
ebrardev <16155052@mersin.edu.tr>
2024-07-27 20:02:54 +0300

    added linear gradient package

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,35 @@
-import {  View,Text } from 'react-native';
+import { View, Text, ImageBackground, SafeAreaView } from 'react-native'
+import React from 'react'
+import  beachImage from "@/assets/meditation-images/beach.webp"
+import { LinearGradient } from 'expo-linear-gradient'
 
-
-
-export default function HomeScreen() {
+const App = () => {
   return (
-   
-    <View className="flex-1 justify-center items-center">
-      <Text className='text-blue-600'>Hello</Text>
+    <View className='flex-1'>
+      <ImageBackground 
+      source={beachImage} 
+      resizeMode="cover" 
+      className='flex-1'
+      >
+
+        <LinearGradient 
+        className='flex-1' 
+        colors={["rgba(0,0,0,0.4)","rgba(0,0,0,0.8)"]}>
+
+
+          <SafeAreaView>
+            <Text>App</Text>
+          </SafeAreaView>
+
+
+
+        </LinearGradient>
+      
+
+      </ImageBackground>
+     
     </View>
-  );
+  )
 }
 
+export default App
