6bcdf360221562a806123d6b75b37cb17f1fb82d
ebrardev <16155052@mersin.edu.tr>
2024-07-28 15:25:09 +0300

    custom button compnent & initiial screen

--- app/index.tsx
+++ app/index.tsx
@@ -2,6 +2,8 @@ import { View, Text, ImageBackground, SafeAreaView } from 'react-native'
 import React from 'react'
 import  beachImage from "@/assets/meditation-images/beach.webp"
 import { LinearGradient } from 'expo-linear-gradient'
+import { StatusBar } from 'expo-status-bar'
+import CustomButton from '@/components/CustomButton'
 
 const App = () => {
   return (
@@ -17,8 +19,18 @@ const App = () => {
         colors={["rgba(0,0,0,0.4)","rgba(0,0,0,0.8)"]}>
 
 
-          <SafeAreaView>
-            <Text>App</Text>
+          <SafeAreaView className='flex-1 mx-5 my-12 justify-between'>
+            <StatusBar style='light'/>
+       <View>
+       <Text className='text-center text-white font-bold text-4xl'>Medidation App</Text>
+       <Text className=' text-center text-white text-regular  text-2xl mt-3'>Meditation for everyone</Text>
+       </View>
+       <View>
+         <CustomButton 
+           onPress={()=>console.log('button pressed')}
+            title='Get Started'
+         />
+       </View>
           </SafeAreaView>
 
 
