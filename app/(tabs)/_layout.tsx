1f838a825e5294c696d3fb0e7292747396272559
ebrardev <16155052@mersin.edu.tr>
2024-07-28 20:52:10 +0300

    refactor: Update tabs layout and add icons for Meditate and Affirmations screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,8 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
 import Colors from '@/constants/Colors'
+import { MaterialCommunityIcons,FontAwesome5 } from '@expo/vector-icons';
+
 
 const TabsLayout = () => {
   return (
@@ -12,7 +14,25 @@ const TabsLayout = () => {
 
     <Tabs.Screen
     name='nature-meditate'
-    options={{tabBarLabel: 'Nature Meditate'}}
+    options={{
+        tabBarLabel: 'Meditate',
+        tabBarIcon:({
+         color 
+        }) =>(
+            <MaterialCommunityIcons name="flower-tulip" size={24} color={color} />
+        )
+    }}
+     />
+      <Tabs.Screen
+    name='affirmations'
+    options={{
+        tabBarLabel: 'Affirmations',
+        tabBarIcon:({
+         color 
+        }) =>(
+            <FontAwesome5 name="book-open" size={24} color={color} />
+        )
+    }}
      />
 
    </Tabs>
