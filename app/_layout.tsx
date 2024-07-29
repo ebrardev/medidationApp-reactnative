4c27d789b70ff103d191f65e794d488c28a64895
ebrardev <16155052@mersin.edu.tr>
2024-07-29 17:38:26 +0300

    route problem fixed

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,14 +3,9 @@ import { Slot, Stack } from "expo-router";
 export default function  RootLayout() {
     return (
        <Stack>
-        <Stack.Screen
-         name="(tabs)" 
-         options={{headerShown: false}}
-          />
-                 <Stack.Screen
-         name="index" 
-         options={{headerShown: false}}
-          />
+        <Stack.Screen name="(tabs)" options={{headerShown: false}} />
+       <Stack.Screen name="index" options={{headerShown: false}}/>
+       <Stack.Screen name='meditate/[id]' options={{headerShown: false}}/>
        </Stack>
     )
 }
---
+++ app/meditate/[id].tsx
@@ -0,0 +1,30 @@
+import { View, Text, ImageBackground, Pressable } from 'react-native'
+import React from 'react'
+import MEDITATION_IMAGES from '@/constants/meditation-images'
+import AppGradient from '@/components/AppGradient'
+import { router } from 'expo-router'
+import { AntDesign } from '@expo/vector-icons';
+
+const Meditate = () => {
+  return (
+    <View className='flex-1 '>
+      <ImageBackground source={MEDITATION_IMAGES[1]} resizeMode='cover' className='flex-1'>
+
+        <AppGradient colors={[ "transparent","rgba(0,0,0,0.8)"]}>
+
+       <Pressable
+        onPress={()=>router.back()}
+        className='absolute top-16 left-6 z-10'
+       >
+         <AntDesign name="leftcircleo" size={40} color="white" />
+       </Pressable>
+
+        </AppGradient>
+
+
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default Meditate
